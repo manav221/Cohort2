1e30c9c29c36070b3b0bee886b8d1081f02cf830
fleet <manavdeora221@gmail.com>
2026-01-24 01:45:17 +0530

    JS DOM to-do

--- script.js
+++ script.js
@@ -1,35 +1,93 @@
-let quotes = [
-    "Aaj kamayin ga toh kal khayin ga",
-    "Ma nhi toh kon bae",
-    "America kya kheta tha",
-    "Aasan haiii",
-    "Oo bhosdi waiter"
-]
-
-
 let main = document.querySelector("main");
-let btn = document.querySelector("button");
+let userInp = document.querySelector("input");
+let addTaskBtn = document.querySelector(".addTask");
+let showTask = document.querySelector(".showTask");
+let iconsClass = ["ri-check-line", "ri-close-line"]
 
-btn.addEventListener("click",function(){
-    let randomIndex = Math.floor(Math.random() * quotes.length);
-    let x = Math.random() * 100;
-    let y = Math.random() * 100;
-    let rotation = Math.random() * 360;
-    let scl = Math.random() * 3;
-    let red = Math.floor(Math.random() * 256);
-    let green = Math.floor(Math.random() * 256);
-    let blue = Math.floor(Math.random() * 256);
+let userTask = ""
+userInp.addEventListener("input", function (event) {
+    userTask = event.target.value;
+})
 
-    let h1 = document.createElement('h1');
 
-    h1.innerHTML = quotes[randomIndex];
 
-    h1.style.color = "#fff";
-    h1.style.position = "absolute";
-    h1.style.top = y + "%";
-    h1.style.left = x + "%";
-    h1.style.rotate = `${rotation}deg`;
-    h1.style.scale = scl;
-    h1.style.color = `rgb(${red},${green},${blue})`
-    main.appendChild(h1);
+addTaskBtn.addEventListener("click", function () {
+
+    if (userTask.trim() === "" || userTask.length > 30) {
+        let errorDisplay = document.createElement("div");
+        errorDisplay.textContent = "Task length can't be less 0 and greater 30";
+        errorDisplay.style.backgroundColor = "#303030";
+        errorDisplay.style.color = "#fff";
+        errorDisplay.style.width = 10 + "rem";
+        errorDisplay.style.height = 10 + "rem";
+        errorDisplay.style.borderRadius = "1rem";
+        errorDisplay.style.fontSize = "0.9rem";
+        errorDisplay.style.display = "flex";
+        errorDisplay.style.alignItems = "center";
+        errorDisplay.style.justifyContent = "center";
+        errorDisplay.style.position = "absolute";
+        errorDisplay.style.zIndex = 99;
+        errorDisplay.style.top = "50%";
+        errorDisplay.style.bottom = "50%";
+        main.appendChild(errorDisplay);
+
+        setTimeout(function(){
+            errorDisplay.remove();
+            userInp.value = "";
+        },2000)
+        return;
+    }
+    let div1 = document.createElement("div");
+    div1.classList.add("task");
+    let li = document.createElement("li");
+    li.textContent = userTask;
+    let div2 = document.createElement("div");
+    div2.className = "taskOptions";
+    let i1 = document.createElement("i");
+    i1.classList.add(iconsClass[0], "option", "markAsDone");
+    let i2 = document.createElement("i");
+    i2.classList.add(iconsClass[1], "option", "Remove");
+
+    showTask.appendChild(div1);
+    div1.appendChild(li);
+    div1.appendChild(div2);
+    div2.appendChild(i1);
+    div2.appendChild(i2);
+
+    userInp.value = "";
+    userTask = "";
+})
+
+userInp.addEventListener("keydown", function (e) {
+    if (e.key === "Enter") {
+        addTaskBtn.click();
+    }
+})
+
+showTask.addEventListener("click", function (event) {
+    let elemClassList = event.target;
+    let parentElem = event.target.parentElement.parentElement;
+    let isCompleted;
+
+    elemClassList.classList.forEach(function (val) {
+        if (val === "markAsDone") {
+            parentElem.classList.forEach(function (val) {
+                if (val === "completed") {
+                    isCompleted = true;
+                }
+            })
+            if (!isCompleted) {
+                parentElem.classList.add("completed");
+
+            } else {
+                parentElem.classList.remove("completed");
+                isCompleted = false;
+            }
+        } else if (val === "Remove") {
+            parentElem.remove();
+        }
+    })
+
+
+
 })
