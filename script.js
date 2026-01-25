86bb6bd4ec4addc00bafc615e5c125bb27204b23
fleet <manavdeora221@gmail.com>
2026-01-25 23:29:01 +0530

    JS DOM Accordion-gpt

--- script.js
+++ script.js
@@ -3,36 +3,26 @@ let faq = document.querySelectorAll(".faq");
 let quest = document.querySelectorAll(".quest");
 let answer = document.querySelector(".answer");
 
-let isOpen = false;
-let temp;
+
+
 quest.forEach(function(val){
-    val.addEventListener("click",function(event){
-        let ans = event.target.parentElement.querySelector("div.answer");
+    val.addEventListener("click",function(){
+        let ans = val.parentElement.querySelector(".answer");
+        let i = val.querySelector("i");
+        let isOpen = ans.style.display === "block";
+
+        document.querySelectorAll(".answer").forEach(function(ans){
+            ans.style.display = "none";
+            val.style.borderBottomLeftRadius = "1rem";
+            val.style.borderBottomRightRadius= "1rem";
+            i.style.display = "none";
+        })
+
         if(!isOpen){
             ans.style.display = "block";
             val.style.borderBottomLeftRadius = 0;
-            val.style.borderBottomRightRadius = 0;
-            isOpen = true;
-            temp = ans;
-        }else if(isOpen && ans === temp){
-            ans.style.display = "none";
-            val.style.borderRadius = "1rem";
-            isOpen = false;
+            val.style.borderBottomRightRadius= 0;
+            i.style.display = "block"
         }
-        else{
-            temp.style.display = "none";
-            val.style.borderRadius = "1rem";
-            isOpen = false;
-            val.click();
-        }
-    })
-
-    val.children[1].addEventListener("click",function(event){
-        if(isOpen){
-            let parent = event.target.parentElement.parentElement;
-            parent.querySelector("div.answer").style.display = 'none';
-            isOpen = false;
-        }
-    })
 })
-
+})
