e3ab0c9a8910978038c98a9d3f9415ebae4f5167
fleet <manavdeora221@gmail.com>
2026-01-26 23:00:30 +0530

    JS DOM timer

--- script.js
+++ script.js
@@ -1,28 +1,60 @@
-let main = document.querySelector("main");
-let faq = document.querySelectorAll(".faq");
-let quest = document.querySelectorAll(".quest");
-let answer = document.querySelector(".answer");
+let timeShow = document.querySelector("h1");
+let mediaBtn = document.querySelector(".play-pause");
+let resetBtn = document.querySelector(".reset");
 
 
+let pauseBtn = document.createElement("i");
+pauseBtn.classList.add("ri-pause-large-line");
+mediaBtn.appendChild(pauseBtn);
+pauseBtn.style.display = "none";
 
-quest.forEach(function(val){
-    val.addEventListener("click",function(){
-        let ans = val.parentElement.querySelector(".answer");
-        let i = val.querySelector("i");
-        let isOpen = ans.style.display === "block";
+let isPlay = false;
 
-        document.querySelectorAll(".answer").forEach(function(ans){
-            ans.style.display = "none";
-            val.style.borderBottomLeftRadius = "1rem";
-            val.style.borderBottomRightRadius= "1rem";
-            i.style.display = "none";
-        })
+let time = 10;
+let rstTime = time;
+let minutes = null;
+let seconds = null;
 
-        if(!isOpen){
-            ans.style.display = "block";
-            val.style.borderBottomLeftRadius = 0;
-            val.style.borderBottomRightRadius= 0;
-            i.style.display = "block"
-        }
+let timer;
+
+function resetTime(Timer){
+    clearInterval(Timer);
+    isPlay = false;
+}
+
+function resetUi(pauseButton,playButton){
+    pauseButton.style.display = "none";
+    playButton.style.display = "block";
+
+}
+mediaBtn.addEventListener("click", function () {
+
+    if (!isPlay) {
+        timer = setInterval(function(){
+            if(time < 0){
+                resetTime(timer);
+                resetUi(pauseBtn,mediaBtn.children[0]);
+                return;
+            }
+            minutes = Math.floor(time / 60);
+            seconds = time % 60;
+            timeShow.textContent = `${minutes < 10 ? '0' + minutes:minutes}:${seconds < 10 ? '0' + seconds:seconds}`;
+            time--;
+        },1000)
+        pauseBtn.style.display = "block";
+        mediaBtn.children[0].style.display = "none";
+        isPlay = true;
+    } else {
+        resetTime(timer);
+        resetUi(pauseBtn,mediaBtn.children[0]);
+    }
+
 })
+resetBtn.addEventListener("click",function(){
+    resetTime(timer);
+    resetUi(pauseBtn,mediaBtn.children[0]);
+    time = rstTime;
+    minutes = Math.floor(time / 60);
+    seconds = time % 60;
+    timeShow.textContent = `${minutes < 10 ? "0" + minutes:minutes}:${seconds < 10 ? "0" + seconds:seconds}`;
 })
