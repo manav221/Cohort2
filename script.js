c78ca3b501f6a48b319696598aefd6a878d3f569
fleet <manavdeora221@gmail.com>
2026-01-27 21:42:50 +0530

    JS-task-3

--- script.js
+++ script.js
@@ -1,60 +1,33 @@
-let timeShow = document.querySelector("h1");
-let mediaBtn = document.querySelector(".play-pause");
-let resetBtn = document.querySelector(".reset");
+let btn = document.querySelector("button");
+let progressBar = document.querySelector(".inner");
+let percent = document.querySelector("h3");
 
-
-let pauseBtn = document.createElement("i");
-pauseBtn.classList.add("ri-pause-large-line");
-mediaBtn.appendChild(pauseBtn);
-pauseBtn.style.display = "none";
-
-let isPlay = false;
-
-let time = 10;
-let rstTime = time;
-let minutes = null;
-let seconds = null;
-
-let timer;
-
-function resetTime(Timer){
-    clearInterval(Timer);
-    isPlay = false;
-}
-
-function resetUi(pauseButton,playButton){
-    pauseButton.style.display = "none";
-    playButton.style.display = "block";
-
-}
-mediaBtn.addEventListener("click", function () {
-
-    if (!isPlay) {
-        timer = setInterval(function(){
-            if(time < 0){
-                resetTime(timer);
-                resetUi(pauseBtn,mediaBtn.children[0]);
-                return;
-            }
-            minutes = Math.floor(time / 60);
-            seconds = time % 60;
-            timeShow.textContent = `${minutes < 10 ? '0' + minutes:minutes}:${seconds < 10 ? '0' + seconds:seconds}`;
-            time--;
-        },1000)
-        pauseBtn.style.display = "block";
-        mediaBtn.children[0].style.display = "none";
-        isPlay = true;
+let grow = 0;
+let isDownloading = false
+btn.addEventListener("click", function () {
+    
+    if (grow >= 100 || !isDownloading) {
+        grow = 0;
+    }
+    
+    if (!isDownloading) {
+        isDownloading = true;
+        let randomNum = 50 + Math.floor(Math.random() * 50);
+        let int = setInterval(function () {
+            grow++;
+            progressBar.style.width = grow + "%";
+            percent.innerHTML = grow + "%";
+        }, randomNum)
+        
+        setTimeout(function () {
+            isDownloading = false;
+            clearInterval(int);
+        }, randomNum * 100)
     } else {
-        resetTime(timer);
-        resetUi(pauseBtn,mediaBtn.children[0]);
+        isDownloading = true;
+        console.log("can't download while downloading");
     }
 
-})
-resetBtn.addEventListener("click",function(){
-    resetTime(timer);
-    resetUi(pauseBtn,mediaBtn.children[0]);
-    time = rstTime;
-    minutes = Math.floor(time / 60);
-    seconds = time % 60;
-    timeShow.textContent = `${minutes < 10 ? "0" + minutes:minutes}:${seconds < 10 ? "0" + seconds:seconds}`;
+
+
 })
