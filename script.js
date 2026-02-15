304817498ba412ec9c090d4d64ebcedae4facb33
fleet <manavdeora221@gmail.com>
2026-02-15 23:20:36 +0530

    adv-css-task-1

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
-document.addEventListener("mousemove",function(dets){
-    document.body.style.setProperty("--x",dets.clientX + "px");
-    document.body.style.setProperty("--y",dets.clientY + "px");
-})
+// const para = document.querySelector("p");
+// const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+// const paraText = para.innerText;
+// let iteration = 0;
+// para.addEventListener("mouseenter",function(dets){
+
+//     let Int = setInterval(function(){
+//         const str = paraText.split("").map(function(char,indx){
+//             if(indx < iteration){
+//                 return char;
+//             }
+//             return chars.split("")[Math.floor(Math.random() * chars.length)]
+//         }).join("");
+//         para.innerText = str;
+//         iteration += 0.1;
+//     },50);
+
+// })
