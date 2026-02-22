56f204584047216e40d744837d88dcb29351e325
fleet <manavdeora221@gmail.com>
2026-02-22 23:47:46 +0530

    JS DOM Snake Game

--- script.js
+++ script.js
@@ -1,168 +1,150 @@
-let userPost = [
-    {
-        profilePic: "https://plus.unsplash.com/premium_photo-1673757121102-0ca51260861f?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fG1vZGVsfGVufDB8fDB8fHww",
-        pendingMsg: 7,
-        location: "USA,California",
-        userName: "Victoria",
-        age: 20,
-        bio: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Id, consequatur quos? Animi quibusdam quasi autem?",
-        hobbies: [
-            {
-                hobby: "Dance",
-                icon: `<i class="ri-quill-pen-fill"></i>`
-            },
-            {
-                hobby: "Code",
-                icon: `<i class="ri-github-line"></i>`
-            },
-            {
-                hobby: "Flute",
-                icon: `<i class="ri-music-fill"></i>`
-            },
-        ],
-        isFriend: false
-    },
-    {
-        profilePic: "https://plus.unsplash.com/premium_photo-1673757094096-be20fd9c8b30?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDIxfHx8ZW58MHx8fHx8",
-        pendingMsg: 3,
-        location: "UK London",
-        userName: "Lisa",
-        age: 21,
-        bio: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Id, consequatur quos? Animi quibusdam",
-        hobbies: [
-            {
-                hobby: "Cooking",
-                icon: `<i class="ri-knife-fill"></i>`
-            },
-            {
-                hobby: "Paint",
-                icon: `<i class="ri-paint-fill"></i>`
-            },
-            {
-                hobby: "Music",
-                icon: `<i class="ri-music-fill"></i>`
-            }],
-        isFriend: false
-    },
-    {
-        profilePic: "https://plus.unsplash.com/premium_photo-1695603438212-522c952b1172?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDU1fHx8ZW58MHx8fHx8",
-        pendingMsg: 6,
-        location: "Aus,Sydney",
-        userName: "Raquel",
-        age: 22,
-        bio: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Id, consequatur quos? Animi",
-        hobbies: [
-            {
-                hobby: "Swim",
-                icon: `<i class="ri-drop-fill"></i>`
-            },
-            {
-                hobby: "Travel",
-                icon: `<i class="ri-ancient-pavilion-fill"></i>`
-            },
-            {
-                hobby: "Doodle",
-                icon: `<i class="ri-pencil-fill"></i>`
-            }
-        ],
-        isFriend: false
-    },
+const gameBoard = document.querySelector(".all-blocks");
+const scrElem = document.querySelector(".score span");
+const snkLength = document.querySelector(".snake-length span");
+let score = 0;
+let direction = "right";
+let foodCordinates = null;
+let isOut = false;
 
-]
-let currPost = 0;
-function select(elem) {
-    return document.querySelector(elem);
-}
-
-function setData(currPost){
-    select(".main-card-img img").src = userPost[currPost].profilePic;
-    select(".nav-bar .dp-img img").src = userPost[currPost].profilePic;
-    select(".badge-icon").textContent = userPost[currPost].pendingMsg;
-    select(".location .place").textContent = userPost[currPost].location;
-    select(".user-name h2").textContent = userPost[currPost].userName;
-    select(".age").textContent = userPost[currPost].age;
-    userPost[currPost].hobbies.forEach(function (hobby) {
-        clutter += `<div class="interest">${hobby.icon} <span>${hobby.hobby}</span></div>`
-    })
-    select(".tags").innerHTML = clutter;
-    clutter = "";
-    select(".bio p").textContent = userPost[currPost].bio;
-}
-let clutter = "";
-let isAnimating = false;
-(function initial() {
-    
-    setData(currPost);
-    select(".incoming-card-img img").src = userPost[currPost + 1].profilePic;
-    currPost = 2;
+const blockHeight = 40;
+const blockWidth = 40;
+const blocks = [];
+let cols = Math.floor(gameBoard.clientWidth / blockWidth);
+let rows = Math.floor(gameBoard.clientHeight / blockHeight);
+(function () {
+    for (let row = 0; row < rows; row++) {
+        for (let col = 0; col < cols; col++) {
+            const div = document.createElement("div");
+            div.classList.add("block");
+            // div.innerText = `${row},${col}`;
+            blocks[`${row},${col}`] = div;
+            gameBoard.appendChild(div);
+        }
+    }
 })();
 
 
+const snake = [
+    { r: 0, c: 0 },
+    { r: 0, c: 1 },
+    { r: 0, c: 2 }
+]
 
-function changeImg() {
-    if (!isAnimating) {
-        isAnimating = true;
-        let tl = gsap.timeline({
-            onComplete: function () {
-                isAnimating = false;
-                let mainCard = select(".main-card-img");
-                let incomingCard = select(".incoming-card-img");
+function showSnake() {
+    snake.forEach(function (elem) {
+        blocks[`${elem.r},${elem.c}`].classList.add("fill");
+    })
+}
 
-                incomingCard.style.zIndex = 3;
-                incomingCard.classList.remove("incoming-card-img");
+function showFood() {
+    const food = { r: Math.floor(Math.random() * rows), c: Math.floor(Math.random() * cols) };
+    blocks[`${food.r},${food.c}`].classList.add("foodColor");
+    return food;
+}
+function eatFood(head) {
+    if (head.c === foodCordinates.c && head.r === foodCordinates.r) {
+        blocks[`${foodCordinates.r},${foodCordinates.c}`].classList.remove("foodColor");
+        foodCordinates = showFood();
+        snakeGrow();
+        increaseScore();
+    }
+}
+function increaseScore() {
+    score += 10;
+    scrElem.textContent = score;
+}
+function snakeGrow() {
+    if (Number(snkLength.textContent) < 12) {
+        let tail = snake[0];
+        snake.unshift({ r: tail.r, c: tail.c });
+        snkLength.textContent = Number(snkLength.textContent) + 1
+    }
 
-                mainCard.style.zIndex = 2;
-                gsap.set(mainCard, {
-                    scale: 1,
-                    opacity: 1
-                })
+}
 
-                if (currPost === userPost.length) currPost = 0;
-                select(".main-card-img img").src = userPost[currPost].profilePic;
-                currPost++;
-                mainCard.classList.remove("main-card-img");
-                incomingCard.classList.add("main-card-img");
-                mainCard.classList.add("incoming-card-img");
-            }
-        })
-        tl.to(".main-card-img", {
-            scale: 1.1,
-            opacity: 0,
-            ease: Circ,
-            duration: .9
-        }, "bkl")
-            .from(".incoming-card-img", {
-                scale: .9,
-                opacity: 0,
-                ease: Circ,
-                duration: .5
-            }, "bkl")
+function Out(head) {
+    if ((head.r < 0 || head.r >= rows) || (head.c < 0 || head.c >= cols)) {
+        isOut = true;
     }
 }
 
-document.querySelector(".post-btns")
-    .addEventListener("click", function (dets) {
-        if (dets.target.classList[0] === "cancel") {
-            changeImg();
-            setData(currPost-1);
-            gsap.from(".details .element", {
-                y: "100%",
-                stagger: 0.06,
-                ease: Power4.easeInOut,
-                duration: .6
-            })
+function selfCollison(head) {
+    for (let i = 0; i < snake.length - 1; i++) {
+        let snakeBody = snake[i];
+        if (snakeBody.r === head.r && snakeBody.c === head.c) {
+            isOut = true;
+            break;
         }
-    })
+    }
+}
 
-
-function createContainers() {
-    document.querySelectorAll(".element").forEach(function (elem) {
-        let div = document.createElement("div");
-        div.classList.add(`${elem.classList[1]}container`);
-        div.style.overflow = "hidden";
-        div.appendChild(elem);
-        document.querySelector(".details").appendChild(div);
+function outScreen() {
+    gameBoard.innerHTML = "";
+    const div = document.createElement("div");
+    const btn = document.createElement("button");
+    btn.innerText = "Restart";
+    div.classList.add("outScreen");
+    div.style.opacity = 1;
+    div.textContent = `Nope,Out!`;
+    div.appendChild(btn);
+    gameBoard.appendChild(div);
+    document.querySelector("button").addEventListener("click", function () {
+        window.location.reload();
     })
 }
-createContainers()
 
+let Int = setInterval(function () {
+    let head = snake[snake.length - 1];
+
+    snake.forEach(function (elem) {
+        blocks[`${elem.r},${elem.c}`].classList.remove("fill");
+    })
+    if (direction === "left") {
+        snake.push({ r: head.r, c: head.c - 1 });
+        head = snake[snake.length - 1];
+        Out(head);
+        selfCollison(head);
+        eatFood(head);
+    } else if (direction === "right") {
+        snake.push({ r: head.r, c: head.c + 1 });
+        head = snake[snake.length - 1];
+        Out(head);
+        selfCollison(head);
+        eatFood(head);
+    } else if (direction === "down") {
+        snake.push({ r: head.r + 1, c: head.c });
+        head = snake[snake.length - 1];
+        Out(head);
+        selfCollison(head);
+        eatFood(head);
+    } else if (direction === "up") {
+        snake.push({ r: head.r - 1, c: head.c });
+        head = snake[snake.length - 1];
+        Out(head);
+        selfCollison(head);
+        eatFood(head);
+    }
+
+    if (!isOut) {
+        snake.shift();
+        showSnake();
+    } else {
+        outScreen();
+        clearInterval(Int);
+    }
+}, 300)
+
+let dispatcher = {
+    "ArrowUp": function () { direction = direction !== "down" ? "up" : direction },
+    "ArrowDown": function () { direction = direction !== "up" ? "down" : direction },
+    "ArrowLeft": function () { direction = direction !== "right" ? "left" : direction },
+    "ArrowRight": function () { direction = direction !== "left" ? "right" : direction },
+}
+document.addEventListener("keydown", function (dets) {
+    if (dispatcher.hasOwnProperty(dets.code)) {
+        dispatcher[dets.code]();
+    }
+})
+
+
+foodCordinates = showFood();
